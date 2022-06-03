661c896bcc8c5282870ac5234e30cab6daeaf362
Madina <madinatussupova0820@gmail.com>
2022-06-03 11:08:19 +0600

    changing styles

--- app.js
+++ app.js
@@ -9,10 +9,18 @@ document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
+
+		//when there's no input
   if (!guess) {
     document.querySelector(".message").textContent = "⛔ No number!";
+				//when player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
+				document.querySelector('body').style.backgroundColor = '#60b347';
+
+				document.querySelector('.number').style.width = '30rem';
+
+				//when guess is higher
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📈 Too high!";
@@ -22,6 +30,7 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "💥 You lost the game!";
 						document.querySelector(".score").textContent = 0;
     }
+				//when guess is lower
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📉 Too low!";
