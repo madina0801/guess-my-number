d3bc116e8b8f8e022e4e5e55ac08d77704a67320
Madina <madinatussupova0820@gmail.com>
2022-06-03 11:50:59 +0600

    again button logic

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 `use strict`;
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -16,6 +14,8 @@ document.querySelector(".check").addEventListener("click", function () {
 				//when player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
+				document.querySelector(".number").textContent = secretNumber;
+
 				document.querySelector('body').style.backgroundColor = '#60b347';
 
 				document.querySelector('.number').style.width = '30rem';
@@ -42,3 +42,14 @@ document.querySelector(".check").addEventListener("click", function () {
     }
   }
 });
+
+document.querySelector('.again').addEventListener('click', function () {
+	score = 20;
+	secretNumber = Math.trunc(Math.random() * 20) + 1;
+	document.querySelector(".message").textContent = "Start quessing...";
+	document.querySelector(".score").textContent = score;
+	document.querySelector(".number").textContent = '?';
+	document.querySelector(".guess").value = '';
+	document.querySelector('body').style.backgroundColor = '#222';
+	document.querySelector('.number').style.width = '15rem';
+})
