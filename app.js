ed41d5362afca80b0af0579d8f3fc42864fff7a0
Madina <madinatussupova0820@gmail.com>
2022-06-03 12:07:43 +0600

    highscore added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -19,6 +20,11 @@ document.querySelector(".check").addEventListener("click", function () {
 				document.querySelector('body').style.backgroundColor = '#60b347';
 
 				document.querySelector('.number').style.width = '30rem';
+
+				if(score > highscore) {
+					highscore = score;
+					document.querySelector('.highscore').textContent = highscore;
+				}
 
 				//when guess is higher
   } else if (guess > secretNumber) {
